1f1dc93e360b470b2a385bdb640eb243ecb76b4b
Maisa Maximo Ferreira <b.outmaself@gmail.com>
2022-05-09 22:58:33 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,17 @@ function scrollActive() {
 
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50;
+    const sectionTop = current.offsetTop - 50
     sectionId = current.getAttribute('id')
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
     } else {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
     }
   })
 }
@@ -45,9 +49,9 @@ window.addEventListener('scroll', scrollActive)
 
 /*==================== SHOW SCROLL TOP ====================*/
 function scrollTop() {
-  const scrollTop = document.getElementById('scroll-top');
+  const scrollTop = document.getElementById('scroll-top')
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 200) scrollTop.classList.add('show-scroll');
+  if (this.scrollY >= 200) scrollTop.classList.add('show-scroll')
   else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
@@ -62,14 +66,20 @@ const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  )
 }
 
 // Activate / deactivate the theme manually with the button
@@ -93,14 +103,14 @@ function removeScale() {
 }
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
-let areaCv = document.getElementById('area-cv');
+let areaCv = document.getElementById('area-cv')
 
 let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
 let opt = {
   margin: 0,
-  filename: 'maisamaximo_resume.pdf',
+  filename: 'lucasrosa_resume.pdf',
   image: {
     type: 'jpeg',
     quality: 0.98
@@ -112,20 +122,20 @@ let opt = {
     format: 'a4',
     orientation: 'portrait'
   }
-};
+}
 
-// Function to call areaCv and Html2Pdf options 
+// Function to call areaCv and Html2Pdf options
 function generateResume() {
-  html2pdf(areaCv, opt);
+  html2pdf(areaCv, opt)
 }
 
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () => {
   // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-  scaleCv();
+  scaleCv()
 
   // 2. The PDF is generated
-  generateResume();
+  generateResume()
 
   // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
   setTimeout(removeScale, 5000)
